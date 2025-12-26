0c7a520d058bc0e6aba4c941abc58a966297cbed
thauan-almeida <thauan.braga-ext@olxbr.com>
2025-12-26 18:41:26 -0300

    Initial Commit

---
+++ NubankClone/NubankCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NubankCloneApp.swift
+//  NubankClone
+//
+//  Created by Thauan Braga Almeida on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NubankCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NubankCloneTests/NubankCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  NubankCloneTests.swift
+//  NubankCloneTests
+//
+//  Created by Thauan Braga Almeida on 26/12/25.
+//
+
+import Testing
+@testable import NubankClone
+
+struct NubankCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
